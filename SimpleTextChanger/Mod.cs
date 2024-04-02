1fa4df3cd76b9549f97578d563df2d94aca6e000
danie <danie@DANIELS-PC2023>
2024-04-02 13:03:50 +1300

    Add project files.

---
+++ SimpleTextChanger/Mod.cs
@@ -0,0 +1,92 @@
+﻿// In Mod.cs
+
+using Colossal.Localization;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using UnityEngine;
+using Colossal.Logging;
+using Colossal.IO.AssetDatabase;
+using static Colossal.IO.AssetDatabase.AtlasFrame;
+
+namespace SimpleTextChanger
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(SimpleTextChanger)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        private Setting m_Setting;
+        public string newText = "TEST";
+        public string entryID_New_Game = "Menu.NEW_GAME";
+        public string Name => "Text Changer"; // Name of your mod
+
+        public string Description => "Changes text in the game"; // Description of your mod
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info("TextChanger mod loaded.");
+
+
+
+            m_Setting = new Setting(this);
+            m_Setting.RegisterInOptionsUI();
+            m_Setting.SetModReference(this);
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+
+            // Prompt user for input and replace text
+            entryID_New_Game = "Menu.NEW_GAME"; // Example entryID
+            newText = GetUserInput("Enter Text:");
+
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+
+
+            // Other code...
+
+            ReplaceText(entryID_New_Game, newText);
+
+        }
+
+
+        public void OnDispose()
+        {
+            ReplaceText(entryID_New_Game, newText);
+
+            log.Info(nameof(OnDispose));
+            if (m_Setting != null)
+            {
+                m_Setting.UnregisterInOptionsUI();
+                m_Setting = null;
+            }
+        }
+
+        public string GetUserInput(string prompt)
+        {
+            // Use the selected dropdown option
+            string selectedOptionText = m_Setting.GetSelectedEnumAsString();
+
+            // Use the selected option in the user prompt
+            return $"{selectedOptionText}";
+        }
+
+        public void ReplaceText(string entryID, string newText)
+        {
+            // Set active locale to modify localization data
+            GameManager.instance.localizationManager.SetActiveLocale("en-US");
+
+            // Check if the entry ID exists in the localization data
+            if (GameManager.instance.localizationManager.activeDictionary.TryGetValue(entryID, out var value))
+            {
+                // Update the value if the entry ID exists
+                GameManager.instance.localizationManager.activeDictionary.Add(entryID, newText, false);
+                log.Info($"TextChanger: Replaced '{entryID}' with '{newText}'");
+            }
+            else
+            {
+                log.Info($"TextChanger: Entry ID '{entryID}' does not exist in the localization data. No changes made.");
+            }
+        }
+    }
+}
---
+++ SimpleTextChanger/Setting.cs
@@ -0,0 +1,158 @@
+﻿using Colossal;
+using Colossal.IO.AssetDatabase;
+using Game.Modding;
+using Game.Settings;
+using Game.UI;
+using Game.UI.Widgets;
+using SimpleTextChanger;
+using System.Collections.Generic;
+using Colossal.UI;
+using System.ComponentModel.Design;
+using Game.SceneFlow;
+using static Colossal.IO.AssetDatabase.AtlasFrame;
+using Unity.Entities;
+using Game.UI.Editor;
+using System.Reflection.Emit;
+
+namespace SimpleTextChanger
+{
+    [FileLocation(nameof(SimpleTextChanger))]
+    [SettingsUIGroupOrder(kDropdownGroup, kTextBoxGroup)]
+    [SettingsUIShowGroupName(kDropdownGroup, kTextBoxGroup)]
+    public class Setting : ModSetting
+    {
+        public const string kSection = "Main";
+        public const string kDropdownGroup = "Dropdown";
+        public const string kButtonGroup = "Button";
+        public const string kTextBoxGroup = "TextBox";
+        private Mod m_Mod;
+
+        // Static variable to hold the selected option
+        // public string OptionValues { get; set; } = "Default Text"; // Default value
+
+        public Setting(IMod mod) : base(mod)
+        {
+            SetDefaults(); // Set default values upon initialization
+
+
+        }
+
+        public void SetModReference(Mod mod)
+        {
+            m_Mod = mod;
+        }
+
+        // Define property for the selected option in the dropdown menu
+
+        // Create a settings UI section for a text box
+
+        [SettingsUISection(kSection, kDropdownGroup)]
+
+        // Define property for the dropdown of strings
+        public SomeEnum EnumDropdown { get; set; } = SomeEnum.New_Game;
+
+
+        [SettingsUISection(kSection, kTextBoxGroup)] // Add a new section for text box
+        public string TextBoxText { get; set; } = "Default Text";
+
+        public override void SetDefaults()
+        {
+            //EnumDropdown = SomeEnum.Value1;
+
+        }
+
+
+        [SettingsUISection(kSection, kButtonGroup)]
+
+        public bool Button_Submit
+        {
+            // When the button is clicked, update newText with the selected value
+            set
+            {
+                Mod.log.Info("Button clicked");
+
+                if (m_Mod != null)
+                {
+                    Mod.log.Info("m_Mod is not null");
+
+                    // Get selected value from the dropdown menu
+                    string selectedValue_New_Game = GetSelectedEnumAsString();
+                    Mod.log.Info($"Selected value: {selectedValue_New_Game}");
+
+                    // Update newText variable in Mod.cs with the selected value
+                    m_Mod.newText = selectedValue_New_Game;
+                    Mod.log.Info("New text updated successfully.");
+
+                    m_Mod.ReplaceText("Menu.NEW_GAME", selectedValue_New_Game);
+                }
+                else
+                {
+                    Mod.log.Error("m_Mod is null. Unable to update new text.");
+                }
+            }
+        }
+
+        public string GetSelectedEnumAsString()
+        {
+            string enumString = EnumDropdown.ToString();
+            // Replace underscores with spaces
+            return enumString.Replace("_", " ");
+        }
+
+        public enum SomeEnum
+        {
+            New_Game,
+            New_Game_Bro,
+            Value3,
+        }  
+    }
+
+
+    public class LocaleEN : IDictionarySource
+    {
+        private readonly Setting m_Setting;
+        public LocaleEN(Setting setting)
+        {
+            m_Setting = setting;
+        }
+        public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
+        {
+            return new Dictionary<string, string>
+            {
+                { m_Setting.GetSettingsLocaleID(), "Simple Text Changer" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
+
+
+
+
+
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.EnumDropdown)), "New Game" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.EnumDropdown)), $"Use any enum property with getter and setter to get enum dropdown" },
+
+                // Add localization for the text box label
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.TextBoxText)), "Enter Text" },
+
+                // Update the settings UI group for the text box
+                { m_Setting.GetOptionGroupLocaleID(Setting.kTextBoxGroup), "Text Boxes" },
+
+                { m_Setting.GetOptionGroupLocaleID(Setting.kDropdownGroup), "Dropdowns" },
+
+                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.New_Game), "New Game" },
+                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.New_Game_Bro), "New game Bro" },
+                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value3), "Value 3" },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Button_Submit)), "Save Changes" },
+
+
+            };
+        }
+
+        public void Unload()
+        {
+
+        }
+    }
+
+
+}
